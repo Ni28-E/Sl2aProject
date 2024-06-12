b30853b6697e1fa7bbdc96f6ec0d03b0d021e268
Heeuw <nickvanelden@gmail.com>
2024-06-12 14:06:07 +0200

    added game selection function on start

--- Sl2aProject/Program.cs
+++ Sl2aProject/Program.cs
@@ -6,9 +6,16 @@ namespace Sl2aProject
     {
         static void Main(string[] args)
         {
-
-            Snake snake = new ();
-            snake.Play();
+            Console.WriteLine("Welcome the great C# game emporium!!!");
+            Console.WriteLine("If you want to play Snake type snake");
+            Console.WriteLine("If you want to play tic-tac-toe type tic");
+            string Game = Console.ReadLine();
+            if (Game == "snake")
+            {
+                Snake snake = new();
+                snake.Play();
+            }
+            
         }
     }
 }
