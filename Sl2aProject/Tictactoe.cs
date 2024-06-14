213a0bf377e2dc8762320a90d80247624de19b0a
Peter <115033260+Horizon26@users.noreply.github.com>
2024-06-14 12:12:04 +0200

    created players

--- Sl2aProject/Tictactoe.cs
+++ Sl2aProject/Tictactoe.cs
@@ -11,48 +11,85 @@ namespace Sl2aProject
         string[,] players =
         {
             {"First player", "x", "Cyan", "0" },
-            {"Seconf player", "o", "Yellow", "0" }
+            {"Second player", "o", "Yellow", "0" }
         };
 
-        char actibeplayer;
+        char activeplayer;
+        int playerInput, turns;
+        char[,] patterns;
+
+
         public Tictactoe()
         {
-            initialize();
+            Initialize();
             display();
             while(true)
             {
                 main();
             }
         }
-        void initialize()
+         public void Initialize()
         {
-
+            this.patterns = Patterns();
+            this.activeplayer = char.Parse(this.players[0,1]);
+            this.playerInput = 0;
+            this.turns = 0;
         }
 
         void display()
         {
-            Console.WriteLine("Main");
+            Console.Clear();
+            Console.WriteLine("\n");
+            DisplayMessage(this.patterns[0, 0]);
+            DisplayMessage(this.patterns[0, 1]);
+            DisplayMessage(this.patterns[0, 2]);
+
+            DisplayMessage(this.patterns[1, 0]);
+            DisplayMessage(this.patterns[1, 1]);
+            DisplayMessage(this.patterns[1, 2]);
+
+            DisplayMessage(this.patterns[2, 0]);
+            DisplayMessage(this.patterns[2, 1]);
+            DisplayMessage(this.patterns[2, 2]);
         }
 
         void main()
         {
-            Console.WriteLine("main");
+            Console.WriteLine("\nmain");
             Console.ReadKey();
         }
 
+        public void DisplayMessage(char patternschar, string message = "")
+        {
+            string stringpatternsChar = char.ToString(patternschar);
+            
+            if(message.Equals(""))
+            {
+                message = " " + patternschar + " ";
+            }
 
+            Console.WriteLine(message);
 
+            bool status = int.TryParse(stringpatternsChar, out int number);
 
+            if(!status)
+            {
+                Console.WriteLine(this.players.GetLength(0));
+                for(int i = 0; i < this.players.GetLength(0); i++) 
+                {
 
+                }
+            }
+        }
 
-
-
-
-
-
-
-
-
-
+        char[,] Patterns()
+        {
+            return new char[,]
+            {
+              {'1','2','3' },
+              {'4','5','6' },
+              {'7','8','9' },
+            };
+        }
     }
 }
